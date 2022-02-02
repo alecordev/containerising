ce52ab95c7a30f778e26addf6f2d9a2ae7456535
alecor Dev <alecor.dev@gmail.com>
2022-02-02 02:14:18 +0000

    Selenium simple example

--- selenium/README.md
+++ selenium/README.md
@@ -1,5 +1,13 @@
 # Selenium
 
+## Simple usage
+
+1. `docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-firefox:4.1.2-20220131`
+2. Point your WebDriver tests to `http://localhost:4444` *
+3. To see what is happening inside the container, head to `http://localhost:7900` (password is secret)
+
+## Notes
+
 - `localhost:4444` to access Selenium grid if using docker-compose.
 - `docker run -d -p 4444:4444 --shm-size="2g" selenium/standalone-firefox:4.1.1-20220121`
 
---
+++ selenium/examples/src/example.py
@@ -0,0 +1,33 @@
+import time
+import subprocess
+from selenium import webdriver
+
+
+def main():
+    print(
+        subprocess.run(
+            'docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" --name firefox-selenium selenium/standalone-firefox:4.1.2-20220131',
+            shell=True
+        )
+    )
+    driver = webdriver.Remote(command_executor="http://localhost:4444")
+
+    try:
+        driver.implicitly_wait(30)
+        driver.maximize_window()
+        driver.get("http://www.python.org")
+        assert "Python" in driver.title
+
+        driver.get("http://rugbychampagneweb.com")
+        time.sleep(5)
+        driver.get("http://artswit.com")
+        time.sleep(10)
+
+    finally:
+        driver.quit()
+        print(subprocess.run("docker stop firefox-selenium", shell=True))
+        print(subprocess.run("docker rm firefox-selenium", shell=True))
+
+
+if __name__ == "__main__":
+    main()
